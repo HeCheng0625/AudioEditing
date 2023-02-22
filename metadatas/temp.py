2276d93e7a36103eccf3eef6bc1145cb147fc881
yuanchengwang <1057041679@qq.com>
2023-02-22 15:06:33 +0000

    commit

--- metadatas/temp.py
+++ metadatas/temp.py
@@ -1,27 +1,61 @@
 import os
 import numpy as np
 
-save_path = "/blob/v-yuancwang/audio_editing_data/replacement_refine/mel"
+save_path = "/blob/v-yuancwang/audio_editing_data/add_refine/mel"
 
-with open("/home/v-yuancwang/AudioEditing/metadata_infos/replacement_refine.txt", "w") as f:
-    with open("/home/v-yuancwang/AudioEditing/metadatas/audioset_fsdesc_replacement_refine.txt", "r") as ff:
+with open("/home/v-yuancwang/AudioEditing/metadata_infos/add_refine.txt", "w") as f:
+    with open("/home/v-yuancwang/AudioEditing/metadatas/audiocaps_add.txt", "r") as ff:
         lines = ff.readlines()
     for line in lines:
-        f1, f2, t1, t2, _ = line.split("   ")
-        f1 = os.path.join(save_path, f1.replace(".wav", ".npy"))
-        f2 = os.path.join(save_path, f2.replace(".wav", ".npy"))
-        t1 = t1.lower().replace("_", " ", 5)
-        t2 = t2.lower().replace("_", " ", 5)
-        t = np.random.choice(["Replace: ", "Replace: ", "Replace:", "replace: ", "Replacement: "]) + t1 + " to " + t2
-        f.write(f1 + "   " + f2 + "   " + t + "\n")
-    with open("/home/v-yuancwang/AudioEditing/metadatas/audioset_fsdesc_replacement_refine_1.txt", "r") as ff:
+        tgt, src, _, _, t, _ = line.split("   ")
+        tgt = os.path.join(save_path, tgt.replace(".wav", ".npy"))
+        src = src.replace(".wav", ".npy")
+        t = t.lower().replace("_", " ", 5)
+        t = np.random.choice(["Add: ", "Add: ", "Add:", "add: "]) + t + " in the background"
+        f.write(src + "   " + tgt + "   " + t + "\n")
+    with open("/home/v-yuancwang/AudioEditing/metadatas/audioset_fsdesc.txt", "r") as ff:
         lines = ff.readlines()
     for line in lines:
-        f1, f2, t1, t2, _ = line.split("   ")
-        f1 = os.path.join(save_path, f1.replace(".wav", ".npy"))
-        f2 = os.path.join(save_path, f2.replace(".wav", ".npy"))
-        t1 = t1.lower().replace("_", " ", 5)
-        t2 = t2.lower().replace("_", " ", 5)
-        t = np.random.choice(["Replace: ", "Replace: ", "Replace:", "replace: ", "Replacement: "]) + t1 + " to " + t2
-        f.write(f1 + "   " + f2 + "   " + t + "\n")
-        
+        tgt, src, _, _, t, type_c = line.split("   ")
+        type_c  = type_c.replace("\n", "")
+        tgt = os.path.join(save_path, tgt.replace(".wav", ".npy"))
+        src = src.replace(".wav", ".npy")
+        t = t.lower().replace("_", " ", 5)
+        if type_c == "0":
+            t = np.random.choice(["Add: ", "Add: ", "Add:", "add: "]) + t + " in the " + np.random.choice(["front", "beginning", "front"])
+        if type_c == "1":
+            t = np.random.choice(["Add: ", "Add: ", "Add:", "add: "]) + t + " in the middle"
+        if type_c == "2":
+            t = np.random.choice(["Add: ", "Add: ", "Add:", "add: "]) + t + " in the end"
+        f.write(src + "   " + tgt + "   " + t + "\n")
+
+# import os
+# import numpy as np
+
+# save_path = "/blob/v-yuancwang/audio_editing_data/add_refine/mel"
+
+# with open("/home/v-yuancwang/AudioEditing/metadata_infos/drop_refine.txt", "w") as f:
+#     with open("/home/v-yuancwang/AudioEditing/metadatas/audiocaps_add.txt", "r") as ff:
+#         lines = ff.readlines()
+#     for line in lines:
+#         src, tgt, _, _, t, _ = line.split("   ")
+#         src = os.path.join(save_path, src.replace(".wav", ".npy"))
+#         tgt = tgt.replace(".wav", ".npy")
+#         t = t.lower().replace("_", " ", 5)
+#         t = np.random.choice(["Drop: ", "Drop: ", "Drop:", "drop: "]) + t
+#         f.write(src + "   " + tgt + "   " + t + "\n")
+#     with open("/home/v-yuancwang/AudioEditing/metadatas/audioset_fsdesc.txt", "r") as ff:
+#         lines = ff.readlines()
+#     for line in lines:
+#         src, tgt, _, _, t, type_c = line.split("   ")
+#         src = os.path.join(save_path, src.replace(".wav", ".npy"))
+#         tgt = tgt.replace(".wav", ".npy")
+#         t = t.lower().replace("_", " ", 5)
+#         # if type_c == "0":
+#         #     t = np.random.choice(["Add: ", "Add: ", "Add:", "add: "]) + t + " in the front"
+#         # if type_c == "1":
+#         #     t = np.random.choice(["Add: ", "Add: ", "Add:", "add: "]) + t + " in the middle"
+#         # if type_c == "2":
+#         #     t = np.random.choice(["Add: ", "Add: ", "Add:", "add: "]) + t + " in the end"
+#         t = np.random.choice(["Drop: ", "Drop: ", "Drop:", "drop: "]) + t
+#         f.write(src + "   " + tgt + "   " + t + "\n")
