0db43e5f4150da5d969aad4c9dfce4b914ad21dc
yuanchengwang <1057041679@qq.com>
2023-02-23 05:21:19 +0000

    commit

--- train_diffusion_wo_p.sh
+++ train_diffusion_wo_p.sh
@@ -1,4 +1,4 @@
-export MODEL_NAME="/blob/v-yuancwang/AudioEditingModel/Diffusion_E_wo_P/checkpoint-10000"
+export MODEL_NAME="/blob/v-yuancwang/AudioEditingModel/Diffusion_E_wo_P/checkpoint-2000"
 export TRAIN_DIR=""
 
 accelerate launch --mixed_precision="no" \
@@ -15,7 +15,7 @@ accelerate launch --mixed_precision="no" \
   --gradient_checkpointing \
   --max_train_steps=1000000 \
   --checkpointing_steps=2000 \
-  --learning_rate=5e-5 \
+  --learning_rate=3e-5 \
   --max_grad_norm=1 \
   --lr_scheduler="constant" --lr_warmup_steps=0 \
   --output_dir="/blob/v-yuancwang/AudioEditingModel/Diffusion_SE"\
